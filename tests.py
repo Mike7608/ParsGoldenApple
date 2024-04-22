b304f4e0e590469043d46e3109dfc3879c94e068
Mike Kushner <mike760807@gmail.com>
2024-04-22 13:06:23 +0300

    init

--- tests.py
+++ tests.py
@@ -9,6 +9,9 @@ from parsGoldApple import ParsGoldApple
 
 class TestMain(unittest.TestCase):
     def test_get_one_item(self):
+        """
+        Тест получения оной записи по заданной ссылке и вывода его на экран
+        """
         # Подготовка данных для теста
         url = "https://goldapple.ru/19000201893-raffaello"
 
@@ -34,12 +37,20 @@ class TestMain(unittest.TestCase):
 class TestParsGoldApple(unittest.TestCase):
 
     def test_get_catalog_links(self):
+        """
+        Тест получения списка ссылок на заданной странице.
+        Внимание!!! Тест может не проходить если какая-либо ссылка на странице изменилась.
+        Чтобы тест прошел обновите список ссылок в переменной test_link
+        """
         pga = ParsGoldApple()
-        test_link = ['https://goldapple.ru/19000201893-raffaello', 'https://goldapple.ru/19000051617-pour-lui', 'https://goldapple.ru/19000191526-magnolia-bouquet', 'https://goldapple.ru/19000041891-402-vanille-caramel-santal', 'https://goldapple.ru/19000148367-rose-magnetic', 'https://goldapple.ru/7192200018-miss-dior-blooming-bouquet', 'https://goldapple.ru/19000220691-winter-delicacies', 'https://goldapple.ru/19000241266-god-spirit', 'https://goldapple.ru/19000241269-holy-jasmine', 'https://goldapple.ru/19000257133-flawless131', 'https://goldapple.ru/19000004549-perfume-vanilla-blend', 'https://goldapple.ru/19000127039-happy-lemon-dulci', 'https://goldapple.ru/19000241268-mumbo-island', 'https://goldapple.ru/19000233252-sienna-brume', 'https://goldapple.ru/19000257139-surfer', 'https://goldapple.ru/19000220687-winter-pralines', 'https://goldapple.ru/19000122575-j-ai-l-air-de-ce-que-je-suis-j-r', 'https://goldapple.ru/19760327528-dylan-turquoise', 'https://goldapple.ru/19000122612-13-30-au-meme-endroit', 'https://goldapple.ru/19000009371-verte-euphorie', 'https://goldapple.ru/19000215889-803-embruns-gingembre-patchouli', 'https://goldapple.ru/19000225170-cuir-curcuma-deluxe-wood-coffret', 'https://goldapple.ru/19760302848-beaute-du-diable', 'https://goldapple.ru/82401200003-l-eau-d-hiver']
         links = pga.get_catalog_links(settings.CATALOG, 1)
+        test_link = ['https://goldapple.ru/19000201893-raffaello', 'https://goldapple.ru/19000051617-pour-lui', 'https://goldapple.ru/19000191526-magnolia-bouquet', 'https://goldapple.ru/19000041891-402-vanille-caramel-santal', 'https://goldapple.ru/19000148367-rose-magnetic', 'https://goldapple.ru/7192200018-miss-dior-blooming-bouquet', 'https://goldapple.ru/19000220691-winter-delicacies', 'https://goldapple.ru/19000241266-god-spirit', 'https://goldapple.ru/19000241269-holy-jasmine', 'https://goldapple.ru/19000257133-flawless131', 'https://goldapple.ru/19000004549-perfume-vanilla-blend', 'https://goldapple.ru/19000127039-happy-lemon-dulci', 'https://goldapple.ru/19000241268-mumbo-island', 'https://goldapple.ru/19000233252-sienna-brume', 'https://goldapple.ru/19000257139-surfer', 'https://goldapple.ru/19000220687-winter-pralines', 'https://goldapple.ru/19000122575-j-ai-l-air-de-ce-que-je-suis-j-r', 'https://goldapple.ru/19760327528-dylan-turquoise', 'https://goldapple.ru/19000122612-13-30-au-meme-endroit', 'https://goldapple.ru/19000009371-verte-euphorie', 'https://goldapple.ru/19000215889-803-embruns-gingembre-patchouli', 'https://goldapple.ru/19000225170-cuir-curcuma-deluxe-wood-coffret', 'https://goldapple.ru/19760302848-beaute-du-diable', 'https://goldapple.ru/82401200003-l-eau-d-hiver']
         self.assertEqual(links, test_link)
 
     def test_get_data_item(self):
+        """
+        Тест получения данных по ссылке
+        """
         pga = ParsGoldApple()
         url = "https://goldapple.ru/19000201893-raffaello"
         data = pga.get_data_item(url)
